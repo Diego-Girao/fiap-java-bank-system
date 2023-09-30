eea64a3ca24e32363c8832acdae454878536e290
diegogirao <diegoramiro25@gmail.com>
2023-09-30 18:37:38 +0100

    feat implemented Serializable and serialVersionUID

--- src/br/com/fiap/banksystem/Person.java
+++ src/br/com/fiap/banksystem/Person.java
@@ -1,7 +1,10 @@
 package br.com.fiap.banksystem;
 
-public class Person {
-private String name;
+import java.io.Serializable;
+
+public class Person implements Serializable {
+    private static final long serialVersionUID = -2307404922933136199L;
+    private String name;
     public String getName() {
         return name;
     }
