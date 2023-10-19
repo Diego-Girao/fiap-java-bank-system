db6666623200efb7f2ae21a10700975344261e1d
diegogirao <diegoramiro25@gmail.com>
2023-10-19 16:11:21 +0100

    update testing class with polimorfism in current account class

--- src/br/com/fiap/banksystem/Test.java
+++ src/br/com/fiap/banksystem/Test.java
@@ -1,15 +1,36 @@
 package br.com.fiap.banksystem;
-
 public class Test {
     public static void main(String[] args) {
-Account account = new Account();
-Account ca = new CurrentAccount();
 
-CurrentAccount account1 = (CurrentAccount) ca;
+        //Método withdraw, sem utilizar polimorfismo
+        Account account1 = new Account();
+        account1.deposit(1500);
+        account1.withdraw(200);
 
-if(account instanceof CurrentAccount){
-    CurrentAccount account2 = (CurrentAccount) account;
-}
+        //Polimorfismo Sobrecrita de método, do método withdraw
+        CurrentAccount account2 = new CurrentAccount();
+        account2.deposit(1700);
+        account2.withdraw(300);
+
+        //Polimorfismo Sobrecrita de método, do método withdraw
+        Account account3 = new CurrentAccount();
+        account3.deposit(600);
+        account3.withdraw(150);
+
+        System.out.println("Saldo conta1: " + account1.getBalance());
+        System.out.println("Saldo conta2: " + account2.getBalance());
+        System.out.println("Saldo conta3: " + account3.getBalance());
+
+        /*// Herança hands-on
+        Account account = new Account();
+        Account ca = new CurrentAccount();
+
+        CurrentAccount account1 = (CurrentAccount) ca;
+
+        if (account instanceof CurrentAccount) {
+            CurrentAccount account2 = (CurrentAccount) account;
+        }*/
+
         /*//        Utilizando o Construtor padrão
         Account cc = new Account();
         cc.deposit(55);
